6eb572c7e3b777033be7bbc8c3abfb1773df71d5
benjanmin huang <benjaminhuanghuang@gmail.com>
2025-12-26 22:39:14 -0800

    update

--- _courses/GraphQL with React-Udemy/Lyrical-GraphQL/client/src/components/SongList.jsx
+++ _courses/GraphQL with React-Udemy/Lyrical-GraphQL/client/src/components/SongList.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import { gql } from "@apollo/client";
+import { useQuery } from "@apollo/client/react";
 
+const GET_SONGS = gql`
+  {
+    songs {
+      id
+      title
+    }
+  }
+`;
 const SongList = () => {
-  return <div>List</div>;
+  const { loading, error, data } = useQuery(GET_SONGS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  return (
+    <div>
+      <h2>Song List</h2>
+      <ul>
+        {data.songs.map((song) => (
+          <li key={song.id}>{song.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default SongList;
