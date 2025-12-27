9d97fd6758e033e6fec9d16c53d1daa1b34b0af1
benjanmin huang <benjaminhuanghuang@gmail.com>
2025-12-26 22:40:07 -0800

    update

---
+++ _code-snippets/SongList.jsx
@@ -0,0 +1,30 @@
+import { gql } from "@apollo/client";
+import { useQuery } from "@apollo/client/react";
+
+const GET_SONGS = gql`
+  {
+    songs {
+      id
+      title
+    }
+  }
+`;
+const SongList = () => {
+  const { loading, error, data } = useQuery(GET_SONGS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  return (
+    <div>
+      <h2>Song List</h2>
+      <ul>
+        {data.songs.map((song) => (
+          <li key={song.id}>{song.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default SongList;
