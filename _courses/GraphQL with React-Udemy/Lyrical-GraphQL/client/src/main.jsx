33e2191a8db4cb9b6a841498029fa14ff28151df
benjanmin huang <benjaminhuanghuang@gmail.com>
2025-12-26 22:34:39 -0800

    update

--- _courses/GraphQL with React-Udemy/Lyrical-GraphQL/client/src/main.jsx
+++ _courses/GraphQL with React-Udemy/Lyrical-GraphQL/client/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client/react";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+const client = new ApolloClient({
+  link: new HttpLink({ uri: "http://localhost:4444/" }),
+  cache: new InMemoryCache(),
+});
+
+createRoot(document.getElementById("root")).render(
+  <ApolloProvider client={client}>
     <App />
-  </StrictMode>,
-)
+  </ApolloProvider>
+);
--- _courses/GraphQL with React-Udemy/client.md
+++ _courses/GraphQL with React-Udemy/client.md
@@ -2,4 +2,21 @@
 
 ```sh
 npm create vite@latest
+
+npm i @apollo/client graphql
+```
+
+```js
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4444/",
+  cache: new InMemoryCache(),
+});
+
+createRoot(document.getElementById("root")).render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
 ```
