b9baefd0b1fc95aad12658776d13343df73393c9
benjaminhuang <benjaminhuanghuang@gmail.com>
2025-12-28 00:22:44 -0800

    graphql auth

--- _courses/GraphQL with React-Udemy/Auth-GraphQL/server.md
+++ _courses/GraphQL with React-Udemy/Auth-GraphQL/server.md
@@ -5,6 +5,7 @@ npm init -y
 
 npm i graphql @apollo/server graphql-tag
 npm i mongoose
+npm i passport
 
 
 ```
--- _courses/GraphQL with React-Udemy/Auth-GraphQL/server/graphql/schema.js
+++ _courses/GraphQL with React-Udemy/Auth-GraphQL/server/graphql/schema.js
@@ -10,8 +10,15 @@ const schema = buildSchema(`
     user: User
   }
 
+  type Mutation {
+    signup(email: String!, password: String!): User
+    login(email: String!, password: String!): User
+    logout: Boolean
+  }
+
   schema {
     query: Query
+    mutation: Mutation
   }
 `);
 
