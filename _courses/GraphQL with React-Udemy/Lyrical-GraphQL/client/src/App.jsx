e1d750471d0f04574052ea87592ad60276ec9a6b
benjaminhuang <benjaminhuanghuang@gmail.com>
2025-12-27 17:15:47 -0800

    update

--- _courses/GraphQL with React-Udemy/Lyrical-GraphQL/client/src/App.jsx
+++ _courses/GraphQL with React-Udemy/Lyrical-GraphQL/client/src/App.jsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 //
 import SongList from "./components/SongList.jsx";
 import SongCreate from "./components/SongCreate.jsx";
+import SongDetail from "./components/SongDetail.jsx";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<SongList />} />
       <Route path="/songs/new" element={<SongCreate />} />
+      <Route path="/songs/:id" element={<SongDetail />} />
     </Routes>
   );
 }
