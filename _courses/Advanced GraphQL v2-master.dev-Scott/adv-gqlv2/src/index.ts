46b6cfd47de5f66f7cbc170e7eae92fd93b8d2ce
benjanmin huang <benjaminhuanghuang@gmail.com>
2026-09-21 11:16:52 -0700

    update

---
+++ _courses/Advanced GraphQL v2-master.dev-Scott/adv-gqlv2/src/index.ts
@@ -0,0 +1,28 @@
+export const typeDefs = `#graphql
+  type User {
+    id: ID!
+    username: String!
+    createdAt: Int!
+  }
+
+  type Settings {
+    user: User!
+    theme: String!
+  }
+
+  input NewSettingsInput {
+    user: ID!
+    theme: String!
+  }
+
+  type Query {
+    me: User!
+    settings(user: ID!): Settings!
+  }
+
+  type Mutation {
+    settings(input: NewSettingsInput!): Settings!
+  }
+`;
+
+const resol;
