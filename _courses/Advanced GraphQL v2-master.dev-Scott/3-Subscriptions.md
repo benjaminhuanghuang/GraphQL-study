9332e305e2b60fbe40498e07dd7c8415fdda026b
benjanmin huang <benjaminhuanghuang@gmail.com>
2026-09-21 23:00:24 -0700

    update

--- _courses/Advanced GraphQL v2-master.dev-Scott/3-Subscriptions.md
+++ _courses/Advanced GraphQL v2-master.dev-Scott/3-Subscriptions.md
@@ -1,1 +1,36 @@
 # Subscriptions
+
+## Real-time Subscriptions
+
+- Subscriptions - A well supported GraphQL operation that's useful for notifying clients of events
+- Live Queries - Client side implementation to be notified when data changes
+
+### Subscriptions vs Live Queries
+
+Subscriptions
+
+- Part of the spec
+- Event observation
+- Great support
+
+Live Queries
+
+- Experimental
+- Data observation
+- Support is getting there
+
+Both
+
+- Flexible transports and protocols
+
+## Adding Subscriptions
+
+- Subscriptions must be added to your Schema like Queries and Mutations
+- Setup PubSub protocol server side
+- Create Subscription event resolvers
+- Add any needed authentication and context
+- Client side setup
+
+## Adding Subscriptions Exercise
+
+## Adding Subscriptions Solution
--- _courses/Advanced GraphQL v2-master.dev-Scott/gql-jwt-auth/src/auth.ts
+++ _courses/Advanced GraphQL v2-master.dev-Scott/gql-jwt-auth/src/auth.ts
@@ -1,4 +1,6 @@
 import jwt from "jsonwebtoken";
+import { GraphQLError } from "graphql";
+import type { GraphQLFieldResolver } from "graphql";
 
 const JWT_SECRET = process.env.JWT_SECRET;
 if (!JWT_SECRET) {
@@ -14,9 +16,7 @@ export interface AuthUser {
 export const createToken = (user: AuthUser): string =>
   jwt.sign({ id: user.id }, JWT_SECRET, { expiresIn: TOKEN_TTL });
 
-export const getUserFromToken = (
-  token?: string | null,
-): AuthUser | null => {
+export const getUserFromToken = (token?: string | null): AuthUser | null => {
   if (!token) return null;
 
   try {
@@ -25,3 +25,43 @@ export const getUserFromToken = (
     return null;
   }
 };
+
+/**
+ * checks if the user is on the context object
+ * continues to the next resolver if true
+ * @param {Function} next next resolver function ro run
+ */
+export const authenticated =
+  <TSource, TContext extends { user: unknown }, TArgs>(
+    next: GraphQLFieldResolver<TSource, TContext, TArgs>,
+  ): GraphQLFieldResolver<TSource, TContext, TArgs> =>
+  (root, args, context, info) => {
+    if (!context.user) {
+      throw new GraphQLError("must authenticate", {
+        extensions: { code: "UNAUTHENTICATED" },
+      });
+    }
+
+    return next(root, args, context, info);
+  };
+
+/**
+ * checks if the user on the context has the specified role.
+ * continues to the next resolver if true
+ * @param {String} role enum role to check for
+ * @param {Function} next next resolver function to run
+ */
+export const authorized =
+  <TSource, TContext extends { user: { role?: string } | null }, TArgs>(
+    role: string,
+    next: GraphQLFieldResolver<TSource, TContext, TArgs>,
+  ): GraphQLFieldResolver<TSource, TContext, TArgs> =>
+  (root, args, context, info) => {
+    if (context.user?.role !== role) {
+      throw new GraphQLError(`you must have ${role} role`, {
+        extensions: { code: "FORBIDDEN" },
+      });
+    }
+
+    return next(root, args, context, info);
+  };
